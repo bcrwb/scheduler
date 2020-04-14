6ce62f88ee11e297d748e1fe534e903f1964c39b
Brent Carey <brentcarey@Brents-MacBook-Air.local>
2020-04-13 22:55:26 -0400

    Finished Listing days 3

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import DayListItem from './DayListItem'
+
+
+export default function DayList({days,day,setDay}){
+   const daysCopy = days.map(dayy => {
+    return <DayListItem 
+    key = {dayy.id}
+    name={dayy.name} 
+    spots={dayy.spots} 
+    selected={dayy.name === day}
+    setDay={setDay}  />
+    
+})
+return <ul>
+    <li>{daysCopy}</li>
+    </ul>
+}
