3db5e1c0cc789d7d6e85af9c5158d2d0920f8976
Brent Carey <brentcarey@Brents-MacBook-Air.local>
2020-04-16 13:46:36 -0400

    Finished selector for days/appointments

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) { 
+    const filteredDays =state.days.filter(dayObj => dayObj.name === day)
+   
+   if(filteredDays.length === 0 || filteredDays === undefined) return []
+   
+   const filteredAppointments = filteredDays[0].appointments.map(id =>  state.appointments[id])
+   return filteredAppointments
+    
+}
+
+// function selectUserByName(state, name) {
+//     const filteredNames = state.users.filter(user => user.name === name);
+//     return filteredNames;
+//   }
